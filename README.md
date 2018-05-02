cbbc9ca1fa5a5705f3a1c7386c678f28d7e5c9ee
Denis Dumulong <dumulong@sbcglobal.net>
2018-05-02 12:25:58 -0500

    Adding comment and fluffing the readme file

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
-# How do I run this application?
+# Feedreader
 
-Just open the index.html file.  At the bottom of the file you will see if all the tests are passing or not.
+This project shows you a list of feeds from different sources.  You click on the hamburger icon to see the list of sources that you can choose from. Happy reading!
+
+## How do I run this application?
+
+Just open the index.html file.
+
+In this version of the project, we have integrated some unit testing into the project.  You'll see the results of those tests at the bottom of the file.  It will show you if the tests are passing or failing.
+
+
+## License
+
+Feedreader is distributed under the MIT license.
--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -1,33 +1,72 @@
 
-$(function() {
+/* feedreader.js
+ *
+ * This is the spec file that Jasmine will read and contains
+ * all of the tests that will be run against your application.
+ */
 
+ /* We're placing all of our tests within the $() function,
+ * since some of these tests may require DOM elements. We want
+ * to ensure they don't run until the DOM is ready.
+ */
+ $(function() {
+
+    /* This is our first test suite - a test suite just contains
+    * a related set of tests. This suite is all about the RSS
+    * feeds definitions, the allFeeds variable in our application.
+    */
     describe('RSS Feeds', function() {
 
+        /* This is our first test - it tests to make sure that the
+         * allFeeds variable has been defined and that it is not
+         * empty. Experiment with this before you get started on
+         * the rest of this project. What happens when you change
+         * allFeeds in app.js to be an empty array and refresh the
+         * page?
+         */
         it('are defined', function() {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
         });
 
+        /* Test that loops through each feed
+         * in the allFeeds object and ensures it has a URL defined
+         * and that the URL is not empty.
+         */
         it('doest not have a blank URL', function() {
-            for (var i = 0; i < allFeeds.length; i++) {
-                expect(allFeeds[i].url).not.toBe('');
-            }
+            allFeeds.forEach(function(feed){
+                expect(feed.url).toBeDefined();
+                expect(feed.url).not.toBe('');
+            });
         });
 
+        /* Test that loops through each feed
+         * in the allFeeds object and ensures it has a name defined
+         * and that the name is not empty.
+         */
         it('doest not have a blank feed name', function() {
-            for (var i = 0; i < allFeeds.length; i++) {
-                expect(allFeeds[i].name).not.toBe('');
-            }
+            allFeeds.forEach(function(feed){
+                expect(feed.name).toBeDefined();
+                expect(feed.name).not.toBe('');
+            });
         });
     });
 
     describe('The menu', function() {
 
+        /* Test that ensures the menu element is
+         * hidden by default.
+         */
         it('start hidden', function() {
             var menu = $('body');
             expect($('body').hasClass('menu-hidden')).toBe(true);
         });
 
+        /* Test that ensures the menu changes
+          * visibility when the menu icon is clicked. This test
+          * has two expectations: the menu display when
+          * clicked and it hide when clicked again.
+          */
         it('is toggled on click', function() {
             var menuIcon = $('.menu-icon-link');
             menuIcon.click();
@@ -40,6 +79,10 @@ $(function() {
 
     describe('Initial Entries', function() {
 
+        /* Test to ensure that, when the loadFeed
+         * function is called and completes its work, there is at least
+         * a single .entry element within the .feed container.
+         */
         beforeEach(function(done) {
             loadFeed(1, done);
         });
@@ -49,6 +92,7 @@ $(function() {
             done();
         });
 
+        /* Return the display back to its original state */
         afterEach(function(done) {
             loadFeed(0, done);
         });
@@ -57,19 +101,23 @@ $(function() {
 
     describe('New Feed Selection', function() {
 
-        var oldList;
+        /* Test that ensures that, when a new feed is loaded
+         * by the loadFeed function, that the content actually changes.
+         */
+        var oldList; //Variable that will contain the list before the change
 
         beforeEach(function(done) {
-            oldList = $('.feed h2');
-            loadFeed(1, done);
+            oldList = $('.feed h2'); //get the current list
+            loadFeed(1, done); //Change the feed
         });
 
         it('should be different after loading new feed', function(done) {
-            var newList = $('.feed h2');
+            var newList = $('.feed h2'); //Now get the new list
             expect(oldList.eq(0).html()).not.toBe(newList.eq(0).html());
             done();
         });
 
+        /* Return the display back to its original state */
         afterEach(function(done) {
             loadFeed(0, done);
         });
